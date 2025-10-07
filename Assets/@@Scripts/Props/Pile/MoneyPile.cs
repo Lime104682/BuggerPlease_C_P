36671c46872601274a0c3afd713b5a5dad018d70
Lime104682 <lime030104@naver.com>
2025-10-07 20:25:27 +0900

    3week_Scripts_add

---
+++ Assets/@@Scripts/Props/Pile/MoneyPile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoneyPile : PileBase
+{
+	public void Awake()
+	{
+		_size = new Vector3(0.2f, 0.1f, 0.2f);
+		_objectType = Define.EObjectType.Money;
+
+	}
+
+}
---
+++ Assets/@@Scripts/UI/WorldSpace/UI_ConstructionArea.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static Define;
+
+[RequireComponent(typeof(WorkerInteraction))]
+public class UI_ConstructionArea : MonoBehaviour
+{
+	[SerializeField]
+	Slider _slider;
+
+	[SerializeField]
+	TextMeshProUGUI _moneyText;
+
+	public UnlockableBase Owner;
+	public long SpentMoney;
+	public long TotalUpgradeMoney;
+	public long MoneyRemaining => TotalUpgradeMoney - SpentMoney;
+
+	void Start()
+    {
+		GetComponent<WorkerInteraction>().OnInteraction = OnWorkerInteraction;
+		GetComponent<WorkerInteraction>().InteractInterval = Define.CONSTRUCTION_UPGRADE_INTERVAL;
+
+		if (Owner == null)
+			Owner = Utils.FindChild<UnlockableBase>(transform.root.gameObject);
+
+		// 잠시 건물은 꺼둠.
+		Owner.gameObject.SetActive(false);
+
+		// TODO : 데이터 참고해서 업그레이드 비용 설정.
+		SpentMoney = 0;
+		TotalUpgradeMoney = 10000;
+
+		RefreshUI();
+	}
+
+    public void OnWorkerInteraction(WorkerController wc)
+	{
+		if (Owner == null)
+			return;
+
+		long money = (long)(TotalUpgradeMoney / (1 / Define.CONSTRUCTION_UPGRADE_INTERVAL));
+
+		if (GameManager.Instance.Money < money)
+			return;
+
+		GameManager.Instance.Money -= money;
+		SpentMoney += money;
+
+		if (SpentMoney >= TotalUpgradeMoney)
+		{
+			SpentMoney = TotalUpgradeMoney;
+
+			// 해금 완료.
+			Owner.gameObject.SetActive(true);
+			gameObject.SetActive(false);
+
+			GameManager.Instance.BroadcastEvent(EEventType.UnlockProp);
+		}
+
+		RefreshUI();
+	}
+
+	void RefreshUI()
+	{
+		_slider.value = SpentMoney / (float)TotalUpgradeMoney;
+		_moneyText.text = Utils.GetMoneyText(MoneyRemaining);
+	}
+}
